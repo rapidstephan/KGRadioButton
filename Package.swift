284c9e5e84327d97377a79ad903b2ab17aa53ba3
SH <sh@mueller-wulff.de>
2020-09-14 12:17:43 +0200

    added SPM support

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "KGRadioButton",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "KGRadioButton",
+            targets: ["KGRadioButton"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "KGRadioButton",
+            dependencies: [],
+            path: "Source"),
+        .testTarget(
+            name: "KGRadioButtonTests",
+            dependencies: ["KGRadioButton"]),
+    ]
+    )
